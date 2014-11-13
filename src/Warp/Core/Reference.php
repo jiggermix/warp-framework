12839f87b5b326d1b28f7ab094f296672bcb5b8b
Jake Josol <jmjosol@hotmail.com>
2014-11-13 17:44:06 +0800

    Fixed configuration reference

--- src/Warp/Core/Reference.php
+++ src/Warp/Core/Reference.php
@@ -36,7 +36,7 @@ class Reference
 		$modelExists 			= strpos($name,"Model") > 0 && static::fileExists("model", $name);
 		$controllerExists 		= strpos($name,"Controller") > 0 && static::fileExists("controller", $name);
 		$viewExists 			= strpos($name,"View") > 0 && static::fileExists("view", $name);
-		$configurationExists 	= strpos($name,"Configuration") > 0 && static::fileExists("configuration", $name));
+		$configurationExists 	= strpos($name,"Configuration") > 0 && static::fileExists("configuration", $name);
 		$migrationExists 		= strncmp("migration", $name, strlen("migration")) == 0 && static::fileExists("migration", $name);
 		
 		if($classExists) 				static::Import("class", $name);
