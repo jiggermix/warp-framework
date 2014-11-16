36bde1ec93fc3878cee71582a09d56f4aa879331
Jake Josol <jmjosol@hotmail.com>
2014-11-17 00:15:20 +0800

    Removd FileHandle::Download

--- src/Warp/Utils/FileHandle.php
+++ src/Warp/Utils/FileHandle.php
@@ -44,38 +44,6 @@ class FileHandle
 		fwrite($this->handle, $text . "\n");
 	}
 
-	public function Download()
-	{
-		ob_end_clean();
-		
-		if (!is_file($this->filename) or connection_status()!=0) return FALSE;
-		
-		header("Cache-Control: no-store, no-cache, must-revalidate");
-		header("Cache-Control: post-check=0, pre-check=0", false);
-		header("Pragma: no-cache");
-		header("Expires: ".gmdate("D, d M Y H:i:s", mktime(date("H")+2, date("i"), date("s"), date("m"), date("d"), date("Y")))." GMT");
-		header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-		header("Content-Type: application/octet-stream");
-		header("Content-Length: ".(string)(filesize($path)));
-		header("Content-Disposition: inline; filename=$name");
-		header("Content-Transfer-Encoding: binary\n");
-		
-		$file = $this->handle;
-
-		if ($file) 
-		{
-		    while(!feof($file) and (connection_status()==0)) 
-		    {
-		    	print(fread($file, 1024*8));
-		    	flush();
-		    }
-		    
-		    fclose($file);
-		}
-		
-		return((connection_status()==0) and !connection_aborted());
-	}
-
 	public function Close()
 	{
 		fclose($this->handle);
