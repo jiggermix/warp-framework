91f74176eef20d019c1b4d529fdfff49254eaf62
Jake Josol <jmjosol@hotmail.com>
2014-11-18 17:37:58 +0800

    Fixed crud view

--- src/Warp/Utils/Traits/CrudViewTrait.php
+++ src/Warp/Utils/Traits/CrudViewTrait.php
@@ -12,12 +12,12 @@ use Warp\Foundation\ViewData;
 
 trait CrudViewTrait
 {
-	protected static CREATE_FILE = "add.php";
-	protected static READ_FILE = "view.php";
-	protected static UPDATE_FILE = "edit.php";
-	protected static DELETE_FILE = "delete.php";
-	protected static PAGE_FILE = "default.php";
-	protected static NUMBER_PATTERN = "[1-9][0-9]*";
+	protected static $CREATE_FILE = "add.php";
+	protected static $READ_FILE = "view.php";
+	protected static $UPDATE_FILE = "edit.php";
+	protected static $DELETE_FILE = "delete.php";
+	protected static $PAGE_FILE = "default.php";
+	protected static $NUMBER_PATTERN = "[1-9][0-9]*";
 	protected $crudFile;
 		
 	public function Crud($type, $parameters=null)
